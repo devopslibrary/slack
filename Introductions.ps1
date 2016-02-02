28b5a539cd4c7c0d4e7d8550bf13abfd0618238c
Ken Erwin <ken.erwin@kinneygroup.com>
2016-02-02 12:13:45 -0500

    Added a bunch of people.

--- Introductions.ps1
+++ Introductions.ps1
@@ -5,80 +5,248 @@ $Intro +=@{name = 'andreww';
            fullName = 'Andrew White';
            currentStatus = 'Site Ops Engineer @ AngiesList.com';
            website = '?';
-           claimToFame = 'Awesome painter, QA, Operations guy with sense of humor'}
+           claimToFame = 'Awesome painter, QA, Operations guy with sense of humor';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'bill.jones';
            fullName = 'Bill Jones';
            currentStatus = 'Lead Database Engineer @ SalesForce.com';
            website = '?';
-           claimToFame = 'Lord of MySQL & Awesome at Operations, formerly AL'}
+           claimToFame = 'Lord of MySQL & Awesome at Operations, formerly AL';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'bradtech';
+           fullName = 'Brad Williamson';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'brandon.olin';
+           fullName = 'Brandon Olin';
+           currentStatus = '?';
+           website = 'http://devblackops.io';
+           claimToFame = '?';
+           twitter = 'devblackops'}
+
+$Intro +=@{name = 'Carlos1Murray';
+           fullName = 'Carlos Murray';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = 'Currently working on entering IT';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'chewster';
            fullName = 'Chase Campos';
            currentStatus = 'Site Reliability Engineer @ AngiesList.com';
            website = '?';
-           claimToFame = 'Super Linux Guy'}
+           claimToFame = '?';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'cyclopsox';
+           fullName = 'Chris Lee';
+           currentStatus = 'Senior ? @ Kinney Group';
+           website = '?';
+           claimToFame = 'Awesome Jack of all Trades guy, big on Jenkins/Linux.';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'chlsmith';
+           fullName = 'Chris Smith';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'cobusbernard';
-           fullName = 'DevOpser with Chef, Ansible, Jenkins.  Also Codes. Playing with Docker';
+           fullName = 'Cobus Bernard';
            currentStatus = 'DevOps @ HealthQ';
            website = 'http://cobus.io/';
-           claimToFame = 'Great at DevOpsy stuff'}
+           claimToFame = 'DevOpser with Chef, Ansible, Jenkins.  Also Codes. Playing with Docker';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'davewyatt';
+           fullName = 'Dave Wyatt';
+           currentStatus = '?';
+           website = 'https://davewyatt.wordpress.com/';
+           claimToFame = 'Chief Pesterer of Pester :D';
+           twitter = 'MSH_Dave'}
 
 $Intro +=@{name = 'bundfx';
            fullName = 'Flynn Bundy';
            currentStatus = 'Systems Engineer @ Coolblue Netherlands';
            website = 'http://flynnbundy.com/';
-           claimToFame = 'A Windows based Systems Engineer currently residing in Rotterdam, The Netherlands.'}
+           claimToFame = 'A Windows based Systems Engineer currently residing in Rotterdam, The Netherlands.';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'gvauter';
            fullName = 'Greg Vauter';
            currentStatus = 'Automation? @ Interactive Intelligence';
            website = 'https://github.com/gvauter';
-           claimToFame = 'Automation Guy? Let me know what to put!'}
+           claimToFame = 'Automation Guy? Let me know what to put!';
+           twitter = 'Unknown'}
 
-$Intro +=@{name = 'hamid.maddi';
+$Intro +=@{name = 'hamid';
            fullName = 'Hamid Maddi';
            currentStatus = 'Automation Engineer @ Interactive Intelligence';
            website = 'N/A';
-           claimToFame = 'Super Openstack/Infrastructure Guy'}
+           claimToFame = 'Super Openstack/Infrastructure Guy';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'jbeaudry';
            fullName = 'Joe Beaudry';
-           currentStatus = '?';
-           website = '?';
-           claimToFame = 'Swiss army knife of random infrastructure skills.'}
+           currentStatus = 'Systems Engineer @ Brainshark';
+           website = 'http://www.ctrlaltshoot.com';
+           claimToFame = 'Swiss army knife of random infrastructure skills.';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'jody.tooley';
            fullName = 'Jody Tooley';
            currentStatus = 'DevOps Manager @ AngiesList.com';
            website = '?';
-           claimToFame = 'Awesome Docker + Jenkins Skills, weak at Starcraft'}
+           claimToFame = 'Awesome Docker + Jenkins Skills, weak at Starcraft';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'josh';
            fullName = 'Joshua Duffney';
            currentStatus = 'Senior Systems Engineer @ ACI Worldwide';
            website = 'https://www.linkedin.com/in/joshuaduffney';
-           claimToFame = 'Powershell guy, gives talks and links us to awesome devops vids'}
+           claimToFame = 'Powershell guy, gives talks and links us to awesome devops vids';
+           twitter = 'Unknown'}
+
+$Intro +=@{name = 'itadder';
+           fullName = 'Justino Garcia';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'jwenzler';
            fullName = '?';
            currentStatus = 'DevOps @ iFit';
            website = 'https://github.com/jwenzler';
-           claimToFame = 'DevOps Guy @ Fitness Company'}
+           claimToFame = 'DevOps Guy @ Fitness Company';
+           twitter = 'Unknown'}
 
 $Intro +=@{name = 'ken';
            fullName = 'Ken Erwin';
            currentStatus = 'Senior Automation Engineer @ Kinney Group';
            website = 'http://www.devopslibrary.com';
-           claimToFame = 'DevOps Library Founder, also Sensu course by OReilly is mine :)'}
+           claimToFame = 'DevOps Library Founder, also Sensu course by OReilly is mine :)';
+           twitter = 'kenerwin88'}
 
-$Intro +=@{name = 'jwenzler';
+$Intro +=@{name = 'martezreed';
+           fullName = 'Martez Reed';
+           currentStatus = 'Senior Automation Engineer ? @ Kinney Group';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'hodge';
+           fullName = 'Matt Hodge';
+           currentStatus = 'DevOps @ Oil Company';
+           website = 'https://hodgkins.io/';
+           claimToFame = 'System Engineer, PowerShell, Chef and Automation lover and Aussie. Living and working in Texas.';
+           twitter = 'matthodge'}
+
+$Intro +=@{name = 'wademikalj';
+           fullName = 'Mikal Wade';
+           currentStatus = 'Site Operations Manager @ Angies List';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'mwrockx';
+           fullName = 'Matt Wrock';
+           currentStatus = '? @ Chef';
+           website = 'http://www.hurryupandwait.io';
+           claimToFame = "Matt Wrock's blog on software development topics related to devops, cloud, automation, .net, powershell, and more.";
+           twitter = 'mwrockx'}
+
+$Intro +=@{name = 'leskat';
+           fullName = 'Nicholas Hansen';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'pat';
+           fullName = 'Patrick Lavalley';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'phil';
+           fullName = 'Phil';
+           currentStatus = 'Site Ops @ Angies List';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'robin';
+           fullName = 'Robin Blanchard';
+           currentStatus = 'Awesome Linux guy @ Nephila Advisors';
+           website = '?';
+           claimToFame = 'Awesome at Big Data/Linux/Storage/Hadoop';
+           twitter = '?'}
+
+$Intro +=@{name = 'rodney';
+           fullName = 'Rodney';
+           currentStatus = '? @ Interactive Intelligence';
+           website = '?';
+           claimToFame = 'Master of VOIP';
+           twitter = '?'}
+
+$Intro +=@{name = 'mcadams';
+           fullName = 'Ryan McAdams';
+           currentStatus = 'DevOps Manager @ Interactive Intelligence';
+           website = '?';
+           claimToFame = 'Openstack + VOIP + Slight minecraft skill';
+           twitter = '?'}
+
+$Intro +=@{name = 'scavallo';
            fullName = '?';
-           currentStatus = 'DevOps @ iFit';
-           website = 'https://github.com/jwenzler';
-           claimToFame = 'DevOps Guy @ Fitness Company'}
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 's.hall';
+           fullName = 'Shawn Hall';
+           currentStatus = 'DevOps @ Indianapolis Company';
+           website = '?';
+           claimToFame = 'PowerShell + Puppet Skills';
+           twitter = '?'}
+
+$Intro +=@{name = 'smurawski';
+           fullName = 'Steven Murawski';
+           currentStatus = 'Software Development Engineer @ Chef';
+           website = 'http://stevenmurawski.com/';
+           claimToFame = 'Total Chef Expert';
+           twitter = 'smurawksi'}
+
+$Intro +=@{name = 'tobmat';
+           fullName = 'Toby Matherly';
+           currentStatus = 'Infra Automation @ Interactive Intelligence';
+           website = '?';
+           claimToFame = 'Openstack Expert';
+           twitter = '?'}
+
+$Intro +=@{name = 'trevorghess';
+           fullName = 'Trevor Hess';
+           currentStatus = '?';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
+
+$Intro +=@{name = 'Zach';
+           fullName = 'Zach';
+           currentStatus = 'Systems Engineer @ ?';
+           website = '?';
+           claimToFame = '?';
+           twitter = '?'}
 
 $Intro | % {
-    "*$($_.fullName)* ($($_.name)) -  ($($_.currentStatus)) -- Website: '''$($_.website)''' $($_.claimToFame)"
+    "*$($_.fullName)* ($($_.name)) -  ($($_.currentStatus)) -- Website: '''$($_.website)''' $($_.claimToFame) (Twitter: $($_.twitter))"
 }
+#$Intro | ConvertTo-Json
